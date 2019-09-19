165fa72c3b612084ebf950c60769f81a574f80fe
Junsong <junsonghuang@hotmail.com>
2019-09-18 21:36:44 -0400

    apriori

--- Apriori.py
+++ Apriori.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import re
+from itertools import combinations
+import pandas as pd
+
 
 def preprocess(): # Three choices pca_a.txt, pca_b.txt, pca_c.txt
     with open('associationruletestdata.txt','r')as f:
@@ -11,10 +13,29 @@ def preprocess(): # Three choices pca_a.txt, pca_b.txt, pca_c.txt
             pca = line.split(sep="\t")
             data_list.append(pca)
         return data_list
+# def set_generation(itemset, length):
 
 def main():
-    dataset = preprocess()
-    print(dataset)
-    # print(dataset)
+    Data = preprocess()
+    Data = pd.DataFrame(Data)
+
+
+    for i in range(len(Data.columns) - 1):
+        Data[i] = 'G' + str(i + 1) + "_" + Data[i].astype(str)
+
+    support = input("Please input the support\t")
+    single_candidate = set()
+
+
+    for i in range(len(Data.columns)):
+        dat_col = Data[i].groupby(Data[i]).describe()
+        for j in range(len(dat_col)):
+            count = list(dat_col.iloc[j])[2:4]
+            if (count[1] >= int(support)):
+                single_candidate.add(count[0])
+    print(single_candidate)
+
+
+
 if __name__ == "__main__":
     main()

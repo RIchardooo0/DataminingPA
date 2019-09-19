4b4f8ca2b39fb14c8bf0befdc51ee92afc5628a7
PaganiniGong <40661586+PaganiniGong@users.noreply.github.com>
2019-09-18 21:22:40 -0400

    Update Apriori.py

--- Apriori.py
+++ Apriori.py
@@ -1,20 +1,54 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import re
-
-def preprocess(): # Three choices pca_a.txt, pca_b.txt, pca_c.txt
-    with open('associationruletestdata.txt','r')as f:
-        data = f.readlines()
-        data_list = []
-        for line in data:
-            pca = line.split()
-            data_list.append(pca)
-        return data_list
-
-def main():
-    dataset = preprocess()
-    print(len(dataset[4]),len(dataset))
-    # print(dataset)
-    print('hello')
-if __name__ == "__main__":
-    main()
+def queryTemp1(template1):
+    parts = eval(template1)
+    if parts[0] == "RULE":
+        print(parts[0])
+        if parts[1] == "ANY":
+            print(parts[1])
+        elif parts[1] == "NONE":
+            print(parts[1])
+        elif parts[1] == 1:
+            print(parts[1])
+    elif parts[0] == "HEAD":
+        print(parts[0])
+        if parts[1] == "ANY":
+            print(parts[1])
+        elif parts[1] == "NONE":
+            print(parts[1])
+        elif parts[1] == 1:
+            print(parts[1])
+    elif parents[0] == "BODY":
+        print(parts[0])
+        if parts[1] == "ANY":
+            print(parts[1])
+        elif parts[1] == "NONE":
+            print(parts[1])
+        elif parts[1] == 1:
+            print(parts[1])
+    return
+def queryTemp2(template2):
+    parts = eval(template2)
+    if parts[0] == "RULE":
+        print(parts[0])
+    elif parts[0] == "HEAD":
+        print(parts[0])
+    elif parents[0] == "BODY":
+        print(parts[0])
+    return
+def queryTemp3(template3):
+    parts = eval(template3)
+    if parts[0] == "1or1":
+        print(parts[0])
+    elif parts[0] == "1and1":
+        print(parts[0])
+    elif parts[0] == "1or2":
+        print(parts[0])
+    elif parts[0] == "1and2":
+        print(parts[0])
+    elif parts[0] == "2or2":
+        print(parts[0])
+    elif parts[0] == "2and2":
+        print(parts[0])
+    return
+a = "asso_rule.template1(\"RULE\", \"ANY\", ['G59_UP', 'G10_Down'])"
+b = "asso_rule.template1(\"RULE\", 1, ['G59_UP', 'G10_Down'])"
+queryOption(b)

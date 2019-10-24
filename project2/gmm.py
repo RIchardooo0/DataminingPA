7a59fa0f663af2a9e56f3a904ef50c9e6615e6c1
PaganiniGong <40661586+PaganiniGong@users.noreply.github.com>
2019-10-23 21:58:24 -0400

    Update gmm.py

--- project2/gmm.py
+++ project2/gmm.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import sys
 
-data = np.loadtxt('cho.txt',delimiter='\t') 
+data = np.loadtxt('cho.txt',delimiter='\t')
 print(data)
-#data = np.loadtxt('iyer.txt',delimiter='\t') 
+#data = np.loadtxt('iyer.txt',delimiter='\t')
 #df = pd.DataFrame(data)
 label = data[:,1]
 clusters = set((data[:,1]))
@@ -29,17 +29,19 @@ def gmm_init(attributes, K):
         covs.append(cov)
     prob_matrix = np.zeros((attributes.shape[0], K))
     return pi, means, covs, prob_matrix
- def gaussian(attributes, pi, means, covs, prob_matrix):
+
+def gaussian(attributes, pi, means, covs, prob_matrix):
     K = len(means)
     for k in range(K):
         for row in range(attributes.shape[0]):
-            dif = (attributes[row] - mean[k])
+            dif = (attributes[row] - means[k])
             covdet = np.linalg.det(covs[k])
             covinv = np.linalg.inv(covs[k])
-            prob = 1.0/(np.power(np.power(2*np.pi*np.abs(covdet),0.5))*np.exp(-0.5*dif.dot(covinv).dot(dif.T)))
+            prob = 1.0/(np.power(2*np.pi*np.abs(covdet),0.5)*np.exp(-0.5*dif.dot(covinv).dot(dif.T)))
             prob_matrix[row][k] = prob
     return prob_matrix
- def s_step(pi, prob_matrix, attributes):
+
+def s_step(pi, prob_matrix, attributes):
     K = len(pi)
     r = np.zeros((attributes.shape[0], K))
     for row in range(attributes.shape[0]):
@@ -48,19 +50,61 @@ def gmm_init(attributes, K):
             denominator = np.sum(pi*prob_matrix[row])
             r[row][k] = numerator/denominator
     return r
- def m_step(pi, prob_matrix, attributes, r, means, covs):
+
+def m_step(pi, prob_matrix, attributes, r, means, covs):
     n = attributes.shape[0]
     cols = attributes.shape[1]
-    u = means
+    pi_new = [0]*len(pi)
     covs_list = []
+    u_list = []
     for k in range(K):
-        pi[k] = np.sum(r[k])/n
-        u[k] = np.sum((r[k].T).dot(attributes))/np.sum(r[k])#not sure
+        pi_new[k] = np.sum(r[k])/n
     for k in range(K):
-        num = []
-        denom = []
+        u = np.zeros((1,cols))
         for row in range(n):
-            num.append(r[row][k]*(attributes[row] - u[k]).dot((attributes[row] - u[k]).T))
-        covs_list.append(np.sum(num)/np.sum(r[k]))
-    return
-    
+            temp = (r[row][k]*attributes[row])
+            u = u + temp
+        u_list.append(u/np.sum(r[k]))
+    for k in range(K):
+        cov = np.zeros((cols,cols))
+        for row in range(n):
+            temp = (r[row][k]*((attributes[row] - u_list[k]).T).dot(attributes[row] - u_list[k]))
+            cov = cov + temp
+        covs_list.append(cov/np.sum(r[k]))
+    return covs_list, u_list, pi_new
+
+K = 5
+pi, means, covs, prob_matrix = gmm_init(attributes, K)
+prob_matrix = gaussian(attributes, pi, means, covs, prob_matrix)
+old_pi = np.array(pi)
+old_means = np.array(means)
+old_covs = np.array(covs)
+r = s_step(pi, prob_matrix, attributes)
+covs_list, u_list, pi_new = m_step(pi, prob_matrix, attributes, r, means, covs)
+new_pi = np.array(pi_new)
+new_means = np.array(u_list)
+new_covs = np.array(covs_list)
+b = 0.00000000001
+while (np.sum(np.abs(new_pi - old_pi)) > b and np.sum(np.abs(new_covs.flatten() - old_covs.flatten()))> b and np.sum(np.abs(new_means - old_means)) > b):
+    old_pi = np.array(pi_new)
+    old_means = np.array(u_list)
+    old_covs = np.array(covs_list)
+    r = s_step(pi_new, prob_matrix, attributes)
+    covs_list, u_list, pi_new = m_step(pi_new, prob_matrix, attributes, r, u_list, covs_list)
+    new_pi = np.array(pi_new)
+    new_means = np.array(u_list)
+    new_covs = np.array(covs_list)
+
+labels = [np.argmax(r[i]) for i in range(attributes.shape[0])]
+print(labels)
+print(sum(labels == label.T)/label.shape[0])
+
+from sklearn.mixture import GaussianMixture 
+data = np.loadtxt('cho.txt',delimiter='\t') 
+df = pd.DataFrame(data[:,2:])
+gmm = GaussianMixture(n_components = 5) 
+gmm.fit(df) 
+labels = gmm.predict(df)
+print(labels)
+print(sum(labels == label.T)/label.shape[0])
+
